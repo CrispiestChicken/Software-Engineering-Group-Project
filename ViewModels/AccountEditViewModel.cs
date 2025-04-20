6725acdf83807718b5f377788a026174dbc71b4b
CrispyChicken <52355245+CrispiestChicken@users.noreply.github.com>
2025-04-20 20:10:39 +0100

    Renamed Individual account page to account edit page

--- ViewModels/AccountEditViewModel.cs
+++ ViewModels/AccountEditViewModel.cs
@@ -1,11 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
-using System.Windows.Input;
 using SftEngGP.Views;
 
 namespace SftEngGP.ViewModels;
 
-internal partial class IndividualAccountViewModel : ObservableObject
+internal partial class AccountEditViewModel : ObservableObject
 {
 
 }
