f6f25a7f408797ad1ab0685a0333658177de06f9
sandy <sandymadill2000@gmail.com>
2025-04-23 18:40:33 +0100

    Added accurcy checks for sensor readings

--- Software-Engineering-Group-Project/ViewModels/SensorViewModel.cs
+++ Software-Engineering-Group-Project/ViewModels/SensorViewModel.cs
@@ -1,5 +1,6 @@
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using Java.Sql;
 using SftEngGP.Database.Data;
 using SftEngGP.Database.Models;
 
@@ -24,7 +25,8 @@ public class SensorViewModel
     public string Unit => _measurand.Unit;
     public string MeasurmentFrequency => _measurand.MeasurmentFrequency;
     public ObservableCollection<SensorReading> SensorReadings { get; set; }
-    
+
+    public int MissingReadingCount => GetMissingReadingCount();
 
     public SensorViewModel(GpDbContext gpDbContext)
     {
@@ -40,5 +42,41 @@ public class SensorViewModel
         _sensor = sensor;
         _measurand = _context.Measurands.Single(m => m.SensorId == _sensor.SensorId);
         SensorReadings = new ObservableCollection<SensorReading>(_context.Readings.Where(r => r.SensorId == _sensor.SensorId));
+    }
+
+    /// <summary>
+    /// Verifies the accuracy of the collected data by determining how many missing readings a sensor has
+    /// </summary>
+    /// <returns>The count of missing readings of the sensor</returns>
+    public int GetMissingReadingCount()
+    {
+        TimeSpan timeSpan = TimeSpan.FromHours(1);
+        DateTimeOffset dateTimeOffset;
+        if (MeasurmentFrequency == "Hourly")
+        {
+            timeSpan = TimeSpan.FromHours(1);
+        }
+        else if (MeasurmentFrequency == "Daily")
+        {
+            timeSpan = TimeSpan.FromDays(1);
+        }
+
+        DateTime timestamp = SensorReadings.First().Timestamp;
+
+        int missedReadings = 0;
+        
+        foreach (SensorReading sensorReading in SensorReadings)
+        {
+            while (sensorReading.Timestamp > timestamp)
+            {
+                missedReadings++;
+                timestamp = timestamp.Add(timeSpan);
+            }
+
+            timestamp = timestamp.Add(timeSpan);
+        }
+
+        return missedReadings;
+
     }
 }
--- Software-Engineering-Group-Project/Views/SensorPage.xaml.cs
+++ Software-Engineering-Group-Project/Views/SensorPage.xaml.cs
@@ -11,6 +11,7 @@ public partial class SensorPage : ContentPage
         InitializeComponent();
         BindingContext = viewModel;
         Trends.Content = new TrendsPage(viewModel);
+        viewModel.GetMissingReadingCount();
 
     }
 }
