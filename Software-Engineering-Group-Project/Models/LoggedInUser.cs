77e57a62ee4645af70ff4a3db5c54d7d782107d5
CrispyChicken <52355245+CrispiestChicken@users.noreply.github.com>
2025-04-30 16:27:09 +0100

    LoggedInUser Class model created

---
+++ Software-Engineering-Group-Project/Models/LoggedInUser.cs
@@ -0,0 +1,56 @@
+﻿using SftEngGP.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SftEngGP.Database.Models;
+
+namespace SftEngGP.Models
+{
+    sealed internal class LoggedInUser
+    {
+
+        private static LoggedInUser? _instance;
+        public string Email { get; set; }
+        public int Role { get; set; }
+
+        private LoggedInUser()
+        {
+        }
+
+        public static LoggedInUser Instance
+        {
+            get
+            {
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new LoggedInUser();
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+
+        public string Login(string email, int roleID)
+        {
+            _instance.Email = email;
+            _instance.Role = roleID;
+
+            return "Logged in";
+        }
+
+
+        public string Logout()
+        {
+            _instance = null;
+            return "Logged out";
+        }
+
+
+
+
+    }
+}
