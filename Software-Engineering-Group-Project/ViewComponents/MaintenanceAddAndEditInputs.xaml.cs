61858e8c8a815fd32a9730781aedb7fb40b7078c
CrispyChicken <52355245+CrispiestChicken@users.noreply.github.com>
2025-05-01 17:14:27 +0100

    Maintainer and sensor inputs for maintenance create page complete

--- Software-Engineering-Group-Project/ViewComponents/MaintenanceAddAndEditInputs.xaml.cs
+++ Software-Engineering-Group-Project/ViewComponents/MaintenanceAddAndEditInputs.xaml.cs
@@ -1,3 +1,5 @@
+using SftEngGP.Database.Models;
+
 namespace SftEngGP.ViewComponents;
 
 public partial class MaintenanceAddAndEditInputs : ContentView
@@ -5,10 +7,30 @@ public partial class MaintenanceAddAndEditInputs : ContentView
     public static readonly BindableProperty CreateOrUpdateProperty =
     BindableProperty.Create(nameof(CreateOrUpdate), typeof(string), typeof(MaintenanceAddAndEditInputs), String.Empty, propertyChanged: OnCreateOrUpdateChanged);
 
+    public static readonly BindableProperty AllSensorsProperty =
+    BindableProperty.Create(nameof(AllSensors), typeof(List<Sensor>), typeof(MaintenanceAddAndEditInputs), null, propertyChanged: OnAllSensorsChanged);
+
+    public static readonly BindableProperty AllAccountsProperty =
+    BindableProperty.Create(nameof(AllAccounts), typeof(List<User>), typeof(MaintenanceAddAndEditInputs), null, propertyChanged: OnAllAccountsChanged);
+
+
+
     public string CreateOrUpdate
     {
         get => (string)GetValue(CreateOrUpdateProperty);
         set => SetValue(CreateOrUpdateProperty, value);
+    }
+
+    public List<Sensor> AllSensors
+    {
+        get => (List<Sensor>)GetValue(AllSensorsProperty);
+        set => SetValue(AllSensorsProperty, value);
+    }
+
+    public List<User> AllAccounts
+    {
+        get => (List<User>)GetValue(AllAccountsProperty);
+        set => SetValue(AllAccountsProperty, value);
     }
 
     private static void OnCreateOrUpdateChanged(BindableObject bindable, object oldValue, object newValue)
@@ -17,6 +39,24 @@ public partial class MaintenanceAddAndEditInputs : ContentView
         var newData = (string)newValue;
 
         control.SubmitButton.Text = newData;
+
+    }
+
+    private static void OnAllSensorsChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var control = (MaintenanceAddAndEditInputs)bindable;
+        var newData = (List<Sensor>)newValue;
+
+        control.SensorSelect.ItemsSource = newData;
+
+    }
+
+    private static void OnAllAccountsChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var control = (MaintenanceAddAndEditInputs)bindable;
+        var newData = (List<User>)newValue;
+
+        control.MaintainerInput.ItemsSource = newData;
 
     }
 
--- Software-Engineering-Group-Project/ViewModels/MaintenanceCreationViewModel.cs
+++ Software-Engineering-Group-Project/ViewModels/MaintenanceCreationViewModel.cs
@@ -1,12 +1,34 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 using SftEngGP.Views;
+using SftEngGP.Database.Models;
+using SftEngGP.Database.Data;
 
 namespace SftEngGP.ViewModels
 {
     internal partial class MaintenanceCreationViewModel : ObservableObject
     {
+
+        private GpDbContext _context;
+        public List<Sensor> AllSensors { get; set; }
+        public List<User> AllAccounts { get; set; }
+
+
+
         [ObservableProperty]
         public string createOrUpdate = "Create";
+
+        [ObservableProperty]
+        public string errorMessage = "";
+
+
+        public MaintenanceCreationViewModel()
+        {
+            _context = new GpDbContext();
+
+            AllSensors = _context.Sensors.ToList();
+            AllAccounts = _context.Users.ToList();
+        }
+
     }
 }
