445651068b53510e209f9fdf7cda1c648a04bd3d
CrispyChicken <52355245+CrispiestChicken@users.noreply.github.com>
2025-05-01 23:42:01 +0100

    Breach class made

---
+++ Software-Engineering-Group-Project/Models/Breach.cs
@@ -0,0 +1,28 @@
+﻿using SftEngGP.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SftEngGP.Models
+{
+    internal class Breach
+    {
+        public float Longitude { get; set; }
+        public float Latitude { get; set; }
+        public string IncidenceType { get; set; }
+        public DateTime DateOfEvent { get; set; }
+        public string Alert { get; set; }
+
+        public Breach(Incidence incident, Sensor sensor)
+        {
+            Longitude = sensor.Longitude;
+            Latitude = sensor.Latitude;
+            IncidenceType = incident.IncidenceType;
+            DateOfEvent = incident.DateOfEvent;
+            Alert = incident.Alert;
+        }
+
+    }
+}
