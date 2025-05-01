a67d1f631b5b5a0f5028202eb679e7b2bc093b58
CrispyChicken <52355245+CrispiestChicken@users.noreply.github.com>
2025-05-02 00:28:22 +0100

    linked breaches to map page

---
+++ SftEngGP.Migrations/Migrations/20250501231510_SensorIDInIncidence.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SftEngGP.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class SensorIDInIncidence : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SensorId",
+                table: "Incidence",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SensorId",
+                table: "Incidence");
+        }
+    }
+}
--- Software-Engineering-Group-Project/ViewModels/MapViewModel.cs
+++ Software-Engineering-Group-Project/ViewModels/MapViewModel.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using SftEngGP.Views;
+using SftEngGP.Database.Data;
+using SftEngGP.Database.Models;
+using System.Collections.ObjectModel;
+using SftEngGP.Models;
 
 namespace SftEngGP.ViewModels
 {
     internal class MapViewModel
     {
+        private GpDbContext _context;
+        public ObservableCollection<Breach> AllBreaches { get; set; }
+        private List<Sensor> _allSensors { get; set; }
+        private List<Incidence> _allIncidents { get; set; }
+
+        public float Longitude { get; set; }
+        public float Latitude { get; set; }
+        public string IncidenceType { get; set; }
+        public DateTime DateOfEvent { get; set; }
+
+        public MapViewModel()
+        {
+            _context = new GpDbContext();
+            _allIncidents = _context.Incidences.ToList();
+            _allSensors = _context.Sensors.ToList();
+
+
+            List<Breach> breaches = new List<Breach>();
+            foreach (var incident in _allIncidents)
+            {
+                var sensor = _allSensors.FirstOrDefault(s => s.SensorId == incident.SensorId);
+                if (sensor != null)
+                {
+                    breaches.Add(new Breach(incident, sensor));
+                }
+            }
+
+            AllBreaches = new ObservableCollection<Breach>(breaches);
+
+
+        }
+
+
     }
 }
