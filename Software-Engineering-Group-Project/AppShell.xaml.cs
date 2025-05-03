9afefa1cd7e15a600a11fc377a0cb0d534a8093f
LeoRojboonthueng <40651296@live.napier.ac.uk>
2025-05-04 00:13:45 +0100

    Added proper routing

--- Software-Engineering-Group-Project/AppShell.xaml.cs
+++ Software-Engineering-Group-Project/AppShell.xaml.cs
@@ -1,9 +1,16 @@
-﻿namespace SftEngGP
+﻿using SftEngGP.Views;
+
+namespace SftEngGP
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
+            
+            Routing.RegisterRoute(nameof(SensorPage), typeof(SensorPage));
+            Routing.RegisterRoute(nameof(TrendsPage), typeof(TrendsPage));
+            Routing.RegisterRoute(nameof(AllSensorsPage), typeof(AllSensorsPage));
+            
             InitializeComponent();
         }
     }
