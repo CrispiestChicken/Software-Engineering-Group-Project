94692612d13279107ad88addc68d5916dc92bf80
LeoRojboonthueng <40651296@live.napier.ac.uk>
2025-05-03 04:44:58 +0100

    Implemented proper database handling

--- SftEngGP.Database/Data/GenericGPDbContext.cs
+++ SftEngGP.Database/Data/GenericGPDbContext.cs
@@ -14,26 +14,26 @@ public abstract class GenericGPDbContext: DbContext {
 
     }
 
-    public GenericGPDbContext(DbContextOptions<GpDbContext> options) : base(options)
+    public GenericGPDbContext(DbContextOptions options) : base(options)
     {
         
     }
     
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        var a = Assembly.GetExecutingAssembly();
-        var resources = a.GetManifestResourceNames();
-        using var stream = a.GetManifestResourceStream("SftEngGP.Database.appsettings.json");
-    
-        var config = new ConfigurationBuilder()
-            .AddJsonStream(stream)
-            .Build();
-    
-        optionsBuilder.UseSqlServer(
-            config.GetConnectionString(connectionName),
-            m => m.MigrationsAssembly("SftEngGP.Migrations")
-        );
+        if (!optionsBuilder.IsConfigured)
+        {
+            var a = Assembly.GetExecutingAssembly();
+            using var stream = a.GetManifestResourceStream("SftEngGP.Database.appsettings.json");
+            var config = new ConfigurationBuilder()
+                .AddJsonStream(stream)
+                .Build();
 
+            optionsBuilder.UseSqlServer(
+                config.GetConnectionString(connectionName),
+                m => m.MigrationsAssembly("SftEngGP.Migrations")
+            );
+        }
     }
 
     public DbSet<SensorReading> Readings { get; set; }
@@ -42,6 +42,7 @@ public abstract class GenericGPDbContext: DbContext {
     public DbSet<Incidence> Incidences { get; set; }
     public DbSet<Measurand> Measurands { get; set; }
     public DbSet<Sensor> Sensors { get; set; }
+
     public DbSet<Update> Updates { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<FrequencyOffset> FrequencyOffsets { get; set; }
--- SftEngGP.Database/Data/GpDbContext.cs
+++ SftEngGP.Database/Data/GpDbContext.cs
@@ -5,7 +5,11 @@ using SftEngGP.Database.Models;
 
 namespace SftEngGP.Database.Data;
 
-public class GpDbContext : GenericGPDbContext
+public class GpDbContext(DbContextOptions<GpDbContext> options) : GenericGPDbContext(options)
 {
+    
+    public DbSet<WaterQuality> WaterQuality { get; set; }
+    public DbSet<AirQuality> AirQuality { get; set; }
+    public DbSet<Weather> Weather { get; set; }
     internal override String connectionName {get; set;} = "DevelopmentConnection";
 }
