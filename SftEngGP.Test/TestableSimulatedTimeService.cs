b91d05e30dc7a58e54f5ed00a138c9382260d511
LeoRojboonthueng <40651296@live.napier.ac.uk>
2025-05-03 04:39:48 +0100

    Added tests & dependencies

---
+++ SftEngGP.Test/TestableSimulatedTimeService.cs
@@ -0,0 +1,23 @@
+﻿namespace SftEngGP.Test;
+
+public class TestableSimulatedTimeService : SimulatedTimeService
+{
+    public TestableSimulatedTimeService(DateTime initialTime) : base(initialTime)
+    {
+        StopAutoAdvance();
+    }
+    
+    private void StopAutoAdvance()
+    {
+        typeof(SimulatedTimeService)
+            .GetField("_timer", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?
+            .SetValue(this, null);
+    }
+
+    public void TriggerTimeChange(DateTime newTime)
+    {
+        typeof(SimulatedTimeService)
+            .GetProperty("SimulatedTime", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?
+            .SetValue(this, newTime);
+    }
+}
---
+++ Software-Engineering-Group-Project/Data/SimulatedTimeSystem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+public class SimulatedTimeService
+{
+    public event Action<DateTime>? OnTimeChanged;
+    private DateTime _simulatedTime;
+    private Timer _timer;
+    
+    public DateTime SimulatedTime
+    {
+        get => _simulatedTime;
+        private set
+        {
+            if (_simulatedTime != value)
+            {
+                _simulatedTime = value;
+                OnTimeChanged?.Invoke(_simulatedTime);
+            }
+        }
+    }
+    
+    public SimulatedTimeService(DateTime dateTime)
+    {
+        _simulatedTime = dateTime;
+
+        _timer = new Timer(1000);
+        _timer.Elapsed += (sender, args) => UpdateTime();
+        _timer.Start();
+    }
+
+    private void UpdateTime()
+    {
+        SimulatedTime = SimulatedTime.AddMinutes(1);
+    }
+}
